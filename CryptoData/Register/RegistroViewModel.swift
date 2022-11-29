6ee906ba0517f33cbe06ccc32a04838166bc1fbb
RodrigoAdauto123 <adauto.rodrigo@pucp.pe>
2022-11-28 20:52:14 -0500

    solid

---
+++ CryptoData/Register/RegistroViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  RegistroViewModel.swift
+//  CryptoData
+//
+//  Created by Rodrigo Adauto Ortiz on 13/11/22.
+//
+
+import UIKit
+
+class RegistroViewModel: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
